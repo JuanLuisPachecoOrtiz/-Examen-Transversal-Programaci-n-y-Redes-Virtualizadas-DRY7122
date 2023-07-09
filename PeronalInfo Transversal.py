97b6329ca59d2d79c7498587a4575295ab669b99
NetAcad DEVASC <changeme@example.com>
2023-07-09 15:52:58 +0000

    SDN

---
+++ PeronalInfo Transversal.py
@@ -0,0 +1,4 @@
+firstName = input("Cual es tu nombre y apellido?")
+Codigo = input("Cual es tu Codigo? ")
+Seccion = input("Cual es tu seccion?")
+print("hola me llamo "  + firstName  , "mi codigo es "  + Codigo   , "mi seccion es  "  + Seccion)
